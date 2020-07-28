347ec71579aaca22daad7178714359f5404e3e71
zariuz <nechetov@hotmail.com>
2020-07-28 19:21:09 +0300

    deploy app

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: 'react-todo-f25e5.firebaseapp.com',
-  databaseURL: 'https://react-todo-f25e5.firebaseio.com',
-  projectId: 'react-todo-f25e5',
-  storageBucket: 'react-todo-f25e5.appspot.com',
-  messagingSenderId: '976960477637',
-  appId: '1:976960477637:web:5e8c9abd1040e33fd0d3ac',
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 const db = firebase.firestore();
